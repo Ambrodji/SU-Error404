52447948adb6501b1ba1cec83d602ef4ff2dacf3
mb <mb@responsfabrikken.dk>
2016-02-29 10:55:57 +0100

    Added go/hello.go

---
+++ go/hello.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+type Server struct {
+  // Nothing to put here yet.
+}
+
+func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    body := "Hello World sker der!\n"
+    // Try to keep the same amount of headers
+    w.Header().Set("Server", "gophr")
+    w.Header().Set("Connection", "keep-alive")
+    w.Header().Set("Content-Type", "text/plain")
+    w.Header().Set("Content-Length", fmt.Sprint(len(body)))
+    fmt.Fprint(w, body)
+}
+
+func main() {
+  server := Server{}
+
+  http.Handle("/", server)
+  if err := http.ListenAndServe(":8081", nil); err != nil {
+    log.Fatal(err)
+  }
+}
