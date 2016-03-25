278b4c06f13d5dbb86e198905a649e300120d228
Mathias Bramming <m@thias.dk>
2016-03-25 16:17:53 +0100

    All empty tables are now initialized properly in DAO

--- backend/src/main/java/dk/error404/dao/Dao.java
+++ backend/src/main/java/dk/error404/dao/Dao.java
@@ -9,19 +9,112 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public abstract class Dao<T> {
-	private final String DATABASE_NAME = "test.db";
+	private final String DATABASE_NAME = "error404database.db";
 	
 	public Dao() {
-		// Initialize tabels in DB in case they don't exist already
+		// Initialize tables in DB in case they don't exist already
 		initializeTabels();
 	}
 	
 	private void initializeTabels() {
-		initializeUsers();
-		//initializeTeams();
+		System.out.println("Initializing DB...");
+		initializeUserTable();
+		initializeTeamTable();
+		initializeTeamParticipantTable();
+		initializeAssignmentTable();
+		initializeQuestionTable();
+		System.out.println("DB loaded.");
 	}
 	
-	private void initializeUsers() {
+	private void initializeTeamTable() {
+		Connection c = null;
+	    Statement stmt = null;
+	    try {
+	    	Class.forName("org.sqlite.JDBC");
+	    	c = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_NAME);
+
+	    	stmt = c.createStatement();
+	    	String sql = "CREATE TABLE IF NOT EXISTS TEAM " +
+	    			"(ID 			INT 	PRIMARY KEY	NOT NULL," +
+	    			" NAME			TEXT	NOT NULL, " + 
+	    			" DESCRIPTION	TEXT); ";
+	    	stmt.executeUpdate(sql);
+	    	stmt.close();
+	    	c.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	    }
+	}
+	
+	private void initializeTeamParticipantTable() {
+		Connection c = null;
+	    Statement stmt = null;
+	    try {
+	    	Class.forName("org.sqlite.JDBC");
+	    	c = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_NAME);
+
+	    	stmt = c.createStatement();
+	    	String sql = "CREATE TABLE IF NOT EXISTS TEAM_PARTICIPANT " +
+	    			"(USER_ID		TEXT	NOT NULL, " +
+	    			" TEAM_ID		INT		NOT NULL, " + 
+	    			" ROLE			TEXT	NOT NULL, " + 
+	    			" PRIMARY KEY (USER_ID, TEAM_ID));";
+	    	stmt.executeUpdate(sql);
+	    	stmt.close();
+	    	c.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	    }
+	}
+	
+	private void initializeAssignmentTable() {
+		Connection c = null;
+	    Statement stmt = null;
+	    try {
+	    	Class.forName("org.sqlite.JDBC");
+	    	c = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_NAME);
+
+	    	stmt = c.createStatement();
+	    	String sql = "CREATE TABLE IF NOT EXISTS TEAM_PARTICIPANT " +
+	    			"(ID 			INT 	PRIMARY KEY	NOT NULL," +
+	    			" NAME			TEXT	NOT NULL, " + 
+	    			" DESCRIPTION	TEXT, " +
+	    			" FEEDBACK		TEXT, " +
+	    			" TEAM_ID		INT		NOT NULL,  " +
+	    			" DEADLINE		TEXT); ";
+	    	stmt.executeUpdate(sql);
+	    	stmt.close();
+	    	c.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	    }
+	}
+	
+	private void initializeQuestionTable() {
+		Connection c = null;
+	    Statement stmt = null;
+	    try {
+	    	Class.forName("org.sqlite.JDBC");
+	    	c = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_NAME);
+
+	    	stmt = c.createStatement();
+	    	String sql = "CREATE TABLE IF NOT EXISTS QUESTION " +
+	    			"(ID 			INT 	PRIMARY KEY	NOT NULL," +
+	    			" ASSIGNMENT_ID	INT		NOT NULL, " + 
+	    			" QUESTION_TEXT	TEXT	NOT NULL, " +
+	    			" QUESTION_TYPE	TEXT	NOT NULL, " +
+	    			" OPTIONS		TEXT, " +
+	    			" ANSWER		TEXT, " +
+	    			" TS_COMPLETED	TEXT); ";
+	    	stmt.executeUpdate(sql);
+	    	stmt.close();
+	    	c.close();
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	    }
+	}
+	
+	private void initializeUserTable() {
 		Connection c = null;
 	    Statement stmt = null;
 	    try {
@@ -30,18 +123,17 @@ public abstract class Dao<T> {
 
 	    	stmt = c.createStatement();
 	    	String sql = "CREATE TABLE IF NOT EXISTS USER " +
-	    			"(ID 			TEXT PRIMARY KEY    NOT NULL," +
+	    			"(ID 			TEXT 	PRIMARY KEY	NOT NULL," +
 	    			" PASSWORD		TEXT	NOT NULL," +
-	    			" NAME          TEXT, " + 
+	    			" NAME			TEXT, " + 
 	    			" EMAIL			TEXT, " +
 	    			" USER_TYPE		TEXT	NOT NULL," +
-	    			" SCHOOL		TEXT)";
+	    			" SCHOOL		TEXT);";
 	    	stmt.executeUpdate(sql);
 	    	stmt.close();
 	    	c.close();
 	    } catch ( Exception e ) {
 	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	      System.exit(0);
 	    }
 	}
 	
--- backend/src/main/java/dk/error404/dao/UserDao.java
+++ backend/src/main/java/dk/error404/dao/UserDao.java
@@ -6,17 +6,12 @@ import java.sql.SQLException;
 import dk.error404.model.User;
 
 public class UserDao extends Dao<User> {
+	public UserDao() {
+		super();
+	}
 
 	@Override
 	public User create(ResultSet result) throws SQLException {
-		/* 
-		 * Dependency dependency = new Dependency();
-        dependency.setLegacyStr(resultSet.getString("legacy_str"));
-        dependency.setGroupId(resultSet.getString("group_id"));
-        dependency.setArtifactId(resultSet.getString("artifact_id"));
-        dependency.setVersion(resultSet.getString("version"));
-        return dependency;
-		 */
 		return null;
 	}
 
