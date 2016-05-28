Ambrodji/SU-Error404

commit f28f306435789bfbcfc6bb9786007abce837ca4c

gameDelete now works properly

backend/WebContent/gameDelete.jsp
@@ -19,30 +19,65 @@
             </div>
             
             <!-- Modal Body -->
-            <div class="modal-body" >
-                
+            <div class="modal-body gameDeleteDiv" >
                 <form id="gameDeleteForm" enctype="multipart/form-data" class="form-horizontal" role="form" method="POST" action="DeleteProg">
-                  <div class="form-group">
-                    <label  class="col-sm-2 control-label"
-                              for="activeGameTitles">Active games</label>
-                    <table class="table table-hover" id="tableDeleteGame">
+                  <div class="form-group noBottomMargin">
+                    <table class="table table-hover noBottomMargin" id="tableDeleteGame">
 						<% ProgramDao dao2 = new ProgramDao(); 
 						ArrayList<Program> programs2 = dao2.findAll();
 						for (Program p : programs2) {%>
-							<tr class="clickable-row" data-href="#">
+							<tr class="clickable-row " data-href='<%=p.getId() + ""%>'>
 								<th><%=p.getName() %></th>
 							</tr>
 						<%}%>
 						<script>
+							$.validator.setDefaults({ ignore: '' });
 							$('#tableDeleteGame').on('click', '.clickable-row', function(event) {
-							  if($(this).hasClass('active')){
-							    $(this).removeClass('active'); 
-							  } else {
-							    $(this).addClass('active').siblings().removeClass('active');
-							  }
+								$('.selected').removeClass('selected');
+								$(this).children(":first").addClass('selected');
+								$("#programId").val($(this).data("href"));
 							});
+				            
+						    $(document).ready(function(){
+						    	$('form#gameDeleteForm').submit(function(event) {
+						        	event.preventDefault();
+						        	jQuery.validator.addMethod("programSelected", function(value, element, param) {
+					            	    return $("#programId").val().length > 0;
+					            	}, "Please select a program to delete");
+				            	    if ($("form#gameDeleteForm").valid()) {
+				            	    	$.ajax({
+				                            url: "DeleteProg",
+				                            type: 'post',
+				                            dataType: 'text',
+				                            data: $("#programId").serialize(),
+				                            success: function(data) {
+				                            	if (data == "success") {
+				                            		alert("The selected program was deleted successfully.");
+				                            	} else {
+				                            		alert("The selected program could not be deleted");
+				                            	}
+				                                
+				                                location.reload(true);
+				                            },
+				                            error: function(data) {
+				                            	alert("The selected program could not be deleted");
+				                            }
+				                    	});
+					            	}
+					            });
+
+				            	$("form#gameDeleteForm").validate({
+				            	    rules: {
+				            	        programId: {
+				            	        	programSelected: true,
+				            	        }
+				            	    },
+				            	    messages: {}
+				            	});
+				            });
 						</script>
 					</table> 
+					<input type="text" name="programId" id="programId" style="display:none;">
                   </div>
                   
                   <!-- Modal Footer -->

backend/WebContent/css/style.css
@@ -164,11 +164,22 @@ body {
     background-color: LightGreen;
 }
 
+.table-hover tbody tr:hover .selected,
+.selected
+{
+    background-color: LightCoral;
+}
+
 .table-hover tbody tr:hover th
 {
-    background-color: LightGreen;
+    background-color: Yellow;
 }
-.table-hover tbody tr clickable-row:active th
+
+.gameDeleteDiv
 {
-    background-color: LightGreen;
+	padding-top: 0px;
+}
+.noBottomMargin
+{
+	margin-bottom: 0px;
 }

backend/src/main/java/dk/error404/servlets/DeleteServlet.java
@@ -43,7 +43,7 @@ public class DeleteServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String programIdStr = request.getParameter("id");
+		String programIdStr = request.getParameter("programId");
 		String userId = (String) request.getSession().getAttribute("user");
 		
 		// User not logged in
@@ -73,10 +73,19 @@ public class DeleteServlet extends HttpServlet {
 		
 		PrintWriter writer = response.getWriter();
 		if (program != null) {
-			dao.delete(program);
-			System.out.println("DeleteServlet: Deleted program with id=" + programId);
-			writer.write(DELETED_SUCCESS);
-			return;
+			File file = new File(program.getFileName());
+			System.out.println("DeleteServlet: Deleting program with path=" + file.getAbsolutePath());
+			
+			if (file.delete()) {
+				System.out.println("DeleteServlet: Deleting program with id=" + programId + " from DB");
+				dao.delete(program);
+				writer.write(DELETED_SUCCESS);
+				return;
+			} else {
+				System.out.println("DeleteServlet: Deletion error. It was not possible to delete program with path="+ file.getAbsolutePath());
+				writer.write(DELETED_ERROR);
+				return;
+			}
 		} else {
 			System.out.println("DeleteServlet: Failed to find program in DB with id=" + programId);
 			writer.write(DELETED_ERROR);

commit b0787f2818351f9620b50e5fbe90e233b856fd53

Fixed regression introduced by f28f306

backend/WebContent/gameDelete.jsp
@@ -31,7 +31,6 @@
 							</tr>
 						<%}%>
 						<script>
-							$.validator.setDefaults({ ignore: '' });
 							$('#tableDeleteGame').on('click', '.clickable-row', function(event) {
 								$('.selected').removeClass('selected');
 								$(this).children(":first").addClass('selected');
@@ -67,9 +66,10 @@
 					            });
 
 				            	$("form#gameDeleteForm").validate({
+				            		ignore: [],
 				            	    rules: {
 				            	        programId: {
-				            	        	programSelected: true,
+				            	        	programSelected: true
 				            	        }
 				            	    },
 				            	    messages: {}